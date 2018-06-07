62c23b5f83ebc4b324990f406108b62f51a7c66a
Jenn Wilde <jennowilde@gmail.com>
2018-06-06 23:04:53 -0500

    finish cleanup of read function

--- index.js
+++ index.js
@@ -5,19 +5,24 @@
 
 var fs = require('fs');
 
+/**
+	* reads a line from the console through the system's TTY file descriptor
+	*
+	* @returns {string} string from the console
+*/
 function read() {
+	// Thank you to the readline-sync package and anseki for guidance
 	var encoding = 'utf8';
-	var bufferSize = 1024;
 	var fileDescriptor = null;
-	var input = '';
-	var atEol;
 	var buffer;
+	var bufferSize = 1024;
 	var readSize;
+	var input = '';
 	var chunk;
-	var line;
+	var atEOL;
 
 	// Attempt to set up read for platform's stdin file descriptor
-	// If we're currently running on windows
+	// If we're currently running on Windows
 	if (process.platform === 'win32') {
 		process.stdin.pause();
 		fileDescriptor = process.stdin.fd;
@@ -28,10 +33,9 @@ function read() {
 		fileDescriptor = fs.openSync('/dev/tty', 'r');
 	}
 
-	// Check `allocUnsafe` to make sure of the new API.
-	buffer = Buffer.allocUnsafe && Buffer.alloc ? Buffer.alloc(bufferSize) : new Buffer(bufferSize);
+	buffer = Buffer.alloc(bufferSize);
 
-	while (true) {
+	do {
 		readSize = fs.readSync(fileDescriptor, buffer, 0, bufferSize);
 
 		if (readSize > 0) {
@@ -41,27 +45,25 @@ function read() {
 			chunk = '';
 		}
 
-		line = (chunk.match(/^(.*)[\r\n]/) || [])[1];
-
-		if (chunk && typeof (line) === 'string') {
-			chunk = line;
-			atEol = true;
+		if (chunk.match(/^(.*)[\r\n]/)) {
+			chunk = (chunk.match(/^(.*)[\r\n]/) || [])[1];
+			atEOL = true;
 		}
 
-		// other ctrl-chars
 		if (chunk) {
 			chunk = chunk.replace(/[\x00-\x08\x0b\x0c\x0e-\x1f\x7f]/g, '');
 			input += chunk;
 		}
-
-		if (readSize === 0 || atEol || input.length >= bufferSize) {
-			break;
-		}
-	}
+	} while (!atEOL && input.length <= bufferSize);
 
 	return input;
 }
 
+/**
+	* writes a line of data to the console through stdout
+	*
+	* @params {any} text
+*/
 function write(text) {
 	process.stdout.write(text);
 }
